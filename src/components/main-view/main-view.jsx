fbae5b334bc6208e170e03a6b2c7cefa40bd0948
JT6100 <97696638+JT6100@users.noreply.github.com>
2022-06-24 22:22:38 -0700

    cleaning code

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
-export class MainView extends React.component {
+export class MainView extends React.Component {
 
     constructor(){
         super();
@@ -13,21 +13,18 @@ export class MainView extends React.component {
     }
 
     render() {
-
-        const { movies } = this.state;
-
-        if (selectedMovie) return <MovieView movie ={selectedMovie} />;
-
-        if (movies.length === 0) return <div className="main-view">The list is Empty!</div>;
-
+        const { movies, selectedMovie } = this.state;
+      
+        if (selectedMovie) return <MovieView movie={selectedMovie} />;
+      
+        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+      
         return (
-            
             <div className="main-view">
-                {movies.map(movie => <MovieCard />)}
+              {movies.map(movie => <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />)}
             </div>
-           
         );
-       }
+      }
 }
 
 export default MainView;
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 export class MovieCard extends React.Component {
     render() {
-        const { movie } = this.props;
-        return <div className='movie-card'>some title</div>;
-    }
+        
+    const { movie } = this.props;
+    return <div className="movie-card" [WE NEED TO ADD onClick HERE]>{movie.Title}</div>;
+  }
 }
