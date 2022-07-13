249c9f7d376081855ff0593c209054cd0063b637
JT6100 <97696638+JT6100@users.noreply.github.com>
2022-07-13 12:45:20 -0700

    changes adding react button and form

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,53 +1,33 @@
-import react from "react";
+import React, { useState} from "react";
+import Form from 'react-bootstrap/Form';
+import { Button } from "react-bootstrap/Button";
 
-export class Loginview extends React.Component
-{
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            username: '',
-            pasword: ''
-        };
-
-        this.onUsernameChange = this.onUsernameChange.bind(this);
-        this.onPasswordChange = this.onPasswordChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    onUsernameChange(event) {
-        this.setState({
-            username: event.target.value
-        });
-    }
-
-    onPasswordChange(event) {
-        this.setState({
-            pasword: event.target.value
-        });
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(username, password);
-        /* Send a request to the server for authentication */
-        /* then call props.onLoggedIn(username) */
-        props.onLoggedIn(username);
-      };
-
-    render() {
-        return (
-            <form>
-                <label>
-                    Username:
-                    <input type="text" value={this.state.username} onChange={this.onUsernameChange} />    
-                </label>
-                <label>
-                    Password:
-                    <input type="password" value={this.state.pasword} onChange={this.onPasswordChange}/>
-                </label>
-                <button type="button" onClick={this.handleSubmit}>Submit</button>
-            </form>
-        );
-    }
-}
+export function LoginView(props) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(username, password);
+      // Send a request to the server for authentication, then call props.onLoggedIn(username)
+      props.onLoggedIn(username);
+    };
+  
+  
+    return (
+      <Form>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+        </Form.Group>
+  
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Form>
+    );
+  }
